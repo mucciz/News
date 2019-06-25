78fbcf0eb290e9c8d5c6f4682e75efcbc12a6320
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-06-25 06:50:36 +0300

    add the two main classes

---
+++ app/models.py
@@ -0,0 +1,25 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country 
+
+class Articles:
+    '''
+    News class to define Article Objects
+    '''
+
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
